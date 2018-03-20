b71787366bc55417a1cc2ac91191a32a090f7e40
Anastasia Lanz <anastasia@heysparkbox.com>
2018-03-20 12:36:11 -0400

    #12 - Flash Messages

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -11,8 +11,8 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   console.log('It worked!');
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
 };
